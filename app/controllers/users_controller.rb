7bfca36798ad85be9f3a900adbdb3500dc205323
julio <julio336@hotmail.com>
2012-07-23 19:11:55 -0500

    Make a basic User model (including secure passwords)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ def show
   end
   def new
   	@user = User.new
-  	
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
